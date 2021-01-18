3406768e3babbb9a6b17f84e7fa087e89694960a
hvtk <hvtk1968@gmail.com>
2021-01-18 08:36:02 +0100

    first setup

---
+++ voorbeeld.js
@@ -0,0 +1,40 @@
+console.log("Yes, hij doet het!");
+console.log("Hallo daar!");
+console.log("Ik zie dat je de console.log functie aan het uitproberen bent!");
+console.log("Zal ik een figuurtje voor je maken?");
+console.log("1, 2, 3...")
+console.log("*****");
+console.log(" ***");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+console.log("  *");
+
+const houseNumber = 12;
+const street = "Prinsenlaan ";
+const address = street + houseNumber;
+
+const score1 = "zeven";
+const score2 = "vier";
+const total = score1 * score2;
+
+console.log(address);
+console.log(total);
+
+if ('hallo') {console.log("Het is waar")} // een string waarde
+//if ('false') {console.log("Het is waar"} // een string waarde waar het woord 'false' in staat
+//if (1) {console.log("Het is waar"} // een numerieke waarde die niet 0 is
+//if ([]) {console.log("Het is waar"} // een lege array
+//if ({}) {console.log("Het is waar"} // een leeg object
+//if (function(){}) {console.log("Het is waar"} // een lege functie
+
+//if (0) {console.log("Het is waar"} // het getal nul
+//if ('') {console.log("Het is waar"} // of "" (een lege string)
+//if (null) {console.log("Het is waar"} // de lege waarde null
+//if (undefined) {console.log("Het is waar"} // niet-toegewezen waarde
+//if (NaN) {console.log("Het is waar"} // Not A Number
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
